be017f74ddc57ffa6a71e33c291cda72609b2c4a
Amet-Vikram <amet1197@gmail.com>
2022-04-06 01:00:05 +0530

    Valid Email ID

--- src/main/java/com/bridgelabz/Main.java
+++ src/main/java/com/bridgelabz/Main.java
@@ -1,21 +1,40 @@
 package com.bridgelabz;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
+    static String[] formArray(){
+        String inValidEMail = "abc, abc@.com.my, abc123@gmail.a, abc123@.com, abc123@.com.com, .abc@abc.com, abc()*@gmail.com, abc@%*.com, abc..2002@gmail.com, abc.@gmail.com, abc@abc@gmail.com, abc@gmail.com.1a";
+        return inValidEMail.split(", ");
+    }
+
+    static String[] formValidArray(){
+        String validEmail = "abc@yahoo.com, abc-100@yahoo.com, abc.100@yahoo.com, abc111@abc.com, abc-100@abc.net, abc.100@abc.com.au, abc@1.com, abc@gmail.com.com, abc+100@gmail.com";
+        return validEmail.split(", ");
+    }
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+//        Scanner scan = new Scanner(System.in);
         Validate validator = new Validate();
 
-        System.out.println("Enter Last Name: ");
-        String name = scan.nextLine();
+        String[] invalidID = formArray();
+        String[] validID = formValidArray();
 
-        if(validator.isValidLastName(name)){
-            System.out.println("Valid");
+
+        System.out.println("Invalid but passed: ");
+        for(String id: invalidID){
+            if(validator.isValidEmail(id)){
+                System.out.println(id);
+            }
         }
-        else{
-            System.out.println("Invalid");
+
+        System.out.println("Valid but did not pass: ");
+        for(String id: validID){
+            if(!validator.isValidEmail(id)){
+                System.out.println(id);
+            }
         }
     }
 }
--- src/main/java/com/bridgelabz/Regexes.java
+++ src/main/java/com/bridgelabz/Regexes.java
@@ -3,11 +3,12 @@ package com.bridgelabz;
 public class Regexes {
     private static Regexes instance = null;
     String validateFirstName = "^[A-Z]\\w{2,12}$";
-    String validateEmail = "\\A[a-z0-9!#$%&'*+/=?^_‘{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_‘{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\z";
+    String validateEmail = "\\A[a-z0-9!#$%&'*+/=?^_‘{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_‘{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z](?:[a-z0-9-]*[a-z0-9])$";
     String validatePhone = "^(\\+\\d{1,3}[- ]?)?\\d{10}$ ";   //  ^(\+\d{1,3}[- ]?)?\d{10}$
     String validatePassword;
     String validateLastName = "^[A-Z]\\w{2,11}$";
 
+//(?:(?=[a-z0-9-]{1,63}\.)[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?=[a-z0-9-]{1,63}\z)[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\z
     private Regexes(){}
 
     public static Regexes getInstance(){
